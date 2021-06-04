1a645cc0bdab2d87b4a1e96cdaf00150bf5d4614
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-04 13:29:29 -0400

    commented out y/n on modification of config.txt

--- install.sh
+++ install.sh
@@ -31,11 +31,11 @@ make
 sudo make install
 cd .. || exit
 
-printf "\n\n"
-printf "In order to run properly, rpitx need to modify /boot/config.txt. Are you sure (y/n) "
-read -r CONT
+#printf "\n\n"
+#printf "In order to run properly, rpitx need to modify /boot/config.txt. Are you sure (y/n) "
+$read -r CONT
 
-if [ "$CONT" = "y" ]; then
+#if [ "$CONT" = "y" ]; then
   echo "Set GPU to 250Mhz in order to be stable"
    LINE='gpu_freq=250'
    FILE='/boot/config.txt'
@@ -44,8 +44,8 @@ if [ "$CONT" = "y" ]; then
    LINE='force_turbo=1'
    grep -qF "$LINE" "$FILE"  || echo "$LINE" | sudo tee --append "$FILE"
    echo "Installation completed !"
-else
-  echo "Warning : Rpitx should be instable and stop from transmitting !";
-fi
+#else
+#  echo "Warning : Rpitx should be instable and stop from transmitting !";
+#fi
 
 
